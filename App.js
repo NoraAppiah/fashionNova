03daf144c9eb49dbccc0922b4c87bc93382a8878
Fashion <amasikanora@icloud.com>
2021-11-13 15:27:17 +0000

    implementing react-native-navigation in app.js

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingPage from './pages/landing';
+import HomePage from './pages/home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="landingpage"
+          component={LandingPage}
+        />
+        <Stack.Screen
+          name="homepage"
+          component={HomePage}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
