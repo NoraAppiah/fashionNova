37cbac6991954948dff52c0eb8041ef05d1d68fb
Fashion <amasikanora@icloud.com>
2021-11-17 14:09:20 +0000

    modified homepage to new design

--- pages/home.js
+++ pages/home.js
@@ -2,40 +2,29 @@ import React from "react";
 import { Dimensions, Image, ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import {Ionicons} from '@expo/vector-icons';
 import { SafeAreaView } from "react-native-safe-area-context";
-import banner from '../assets/banner.jpg';
-import product from '../assets/product.png';
+import db from "../db";
 const size = Dimensions.get("screen");
 
 export default function HomePage({navigation}){
-    const products = [0,1,3,4,5,6,7];
-
     return <View style={{flex: 1}}>
-        <SafeAreaView edges={["top"]} />
-        <View style={styles.homeHeader}>
-            <Ionicons size={25} name="ios-menu-outline" />
-            <Text style={{fontSize: 20, fontWeight: "bold"}}>HOME</Text>
-            <Ionicons size={25} name="ios-menu-outline" color="transparent" />
+        <View style={{backgroundColor: "rgba(196, 155, 156, 0.5)"}}>
+            <SafeAreaView edges={["top"]} />
+            <View style={[styles.homeHeader]}>
+                <Ionicons size={25} name="ios-menu-outline" />
+                <Text style={{fontSize: 20, fontWeight: "bold"}}>HOME</Text>
+                <Ionicons size={25} name="ios-cart-outline" />
+            </View>
         </View>
         <ScrollView style={{flex: 1, paddingHorizontal: 20}}>
-            <View style={styles.bannerArea}>
-                <Image source={banner} style={styles.banner} />
-                <View style={styles.bannerText}>
-                    <Text style={{fontSize: 25, fontWeight: "bold"}}>LOOK</Text>
-                    <Text style={{fontSize: 35, fontWeight: "bold"}}>LIKE</Text>
-                    <Text style={{fontSize: 25, fontWeight: "bold"}}>A Star</Text>
-                </View>
-            </View>
             <View style={{flexDirection: "row", flexWrap: "wrap"}}>
-                {products.map((item, index)=> 
-                <TouchableWithoutFeedback onPress={()=> navigation.navigate("productdetail")} key={index}>
+                {db.items.map((item, index)=> 
+                <TouchableWithoutFeedback onPress={()=> navigation.navigate("productdetail", {item})} key={index}>
                     <View style={[styles.product, {marginRight: (index+1)%2 === 0 ? 0 : 10}]}>
-                        <Image source={product} style={{width: "100%", height: 220}} />
-                        <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between", padding: 5}}>
-                            <View style={{flexDirection: "row", alignItems: "center"}}>
-                                <Text style={styles.originalPrice}>&cent;25.00</Text>
-                                <Text style={styles.price}>&cent;15.00</Text>
-                            </View>
-                            <Ionicons name="ios-heart-outline" size={20} />
+                        <Image source={item.images[0].source} style={{width: "100%", height: 220}} />
+                        <View style={{padding: 5}}>
+                            <Text style={{fontSize: 14, marginBottom: 2, fontWeight: "bold", textTransform: "uppercase"}}>{item.name}</Text>
+                            <Text style={{fontSize: 11, marginBottom: 3 }}>{item.shortDesc}</Text>
+                            <Text style={styles.originalPrice}>&cent;{item.price.toFixed(2)}</Text>
                         </View>
                     </View>
                 </TouchableWithoutFeedback>
@@ -77,24 +66,18 @@ const styles = StyleSheet.create({
     },
     product: {
         width: (size.width - 50)/2,
-        marginBottom: 10
+        marginTop: 10
     },
     originalPrice: {
         fontSize: 14, 
-        color: "#aaa",
-        marginRight: 5,
         fontWeight: "bold"
-    },
-    price: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "orangered"
     },
     bottomNavigation: {
         padding: 20,
         paddingTop: 15,
         flexDirection: "row",
         justifyContent: "space-between",
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: "rgba(196, 155, 156, 0.5)"
     }
 })
